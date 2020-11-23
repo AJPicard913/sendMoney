aafe2214b7e3573f5fa36ad52c8f603be82e0910
ajpicard913 <Contact@saucedesigns.io>
2020-11-23 02:54:22 -0500

    Initial Commit

---
+++ Shared/SendMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendMoneyApp.swift
+//  Shared
+//
+//  Created by AJ Picard on 11/23/20.
+//
+
+import SwiftUI
+
+@main
+struct SendMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
